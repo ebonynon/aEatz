b8e1411c1bd4e10b55668eb0f6516e1c2c6f5f54
ebonynon <sithumsandeepap@gmail.com>
2020-12-08 09:59:08 +0530

    navbar init

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Router, Route, Switch } from "react-router-dom";
 // pages for this product
 import HomePage from "./pages/HomePage/HomePage";
 
+// core components for this product
+import NavBar from "./components/common/Navbar";
+
 const hist = createBrowserHistory();
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Router history={hist}>
         <Switch>
           <Route path="/" component={HomePage} />
