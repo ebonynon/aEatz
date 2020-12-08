97f6231e92be9f8ed8113f53d012694a0b8e7c6e
ebonynon <sithumsandeepap@gmail.com>
2020-12-08 12:19:28 +0530

    navbar ~0

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,18 +1,18 @@
 import {
   Flex,
   Stack,
-  PseudoBox,
+  Box,
   useColorMode,
   IconButton,
-  Box,
   Image,
-} from "@chakra-ui/core";
+  Link,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const bgColor = { light: "gray.300", dark: "gray.600" };
   const textColor = { light: "black", dark: "gray.100" };
-  const router = useRouter();
   return (
     <Flex
       w="100vw"
@@ -28,7 +28,7 @@ export default function NavBar() {
     >
       <Flex w={["100vw", "100vw", "80vw", "80vw"]} justify="space-around">
         <Box>
-          <Image h="4vh" src="./logo.svg" alt="Logo of Chakra-ui" />
+          <Image h="4vh" src="../../logo.svg" alt="Logo of Chakra-ui" />
         </Box>
         <Stack
           spacing={8}
@@ -37,44 +37,44 @@ export default function NavBar() {
           align="center"
           isInline
         >
-          <PseudoBox
+          <Box
             position="relative"
-            opacity={router.pathname !== "/" ? 0.4 : 1}
+            opacity={window.location.pathname !== "/" ? 0.4 : 1}
           >
             <Link href="/">
               <a>Home</a>
             </Link>
-          </PseudoBox>
-          <PseudoBox
+          </Box>
+          <Box
             position="relative"
-            opacity={router.pathname !== "/form" ? 0.4 : 1}
+            opacity={window.location.pathname !== "/form" ? 0.4 : 1}
           >
-            <Link href="/form">
+            <Link to="/form">
               <a>Form</a>
             </Link>
-          </PseudoBox>
-          <PseudoBox
+          </Box>
+          <Box
             position="relative"
-            opacity={router.pathname !== "/card" ? 0.4 : 1}
+            opacity={window.location.pathname !== "/card" ? 0.4 : 1}
           >
             <Link href="/card">
               <a>Card</a>
             </Link>
-          </PseudoBox>
-          <PseudoBox
+          </Box>
+          <Box
             position="relative"
-            opacity={router.pathname !== "/list" ? 0.4 : 1}
+            opacity={window.location.pathname !== "/list" ? 0.4 : 1}
           >
             <Link href="/list">
               <a>List</a>
             </Link>
-          </PseudoBox>
+          </Box>
         </Stack>
         <Box>
           <IconButton
             rounded="full"
             onClick={toggleColorMode}
-            icon={colorMode === "light" ? "moon" : "sun"}
+            icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
           >
             Change Color Mode
           </IconButton>
