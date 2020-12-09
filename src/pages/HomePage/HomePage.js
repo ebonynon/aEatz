1a076c3ae2f68aaa1c92f4f384c4d99de952c7cb
ebonynon <sithumsandeepap@gmail.com>
2020-12-09 11:19:12 +0530

    set useQuery to get data

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+//import { RestaurantsService } from "../../services";
+import { environment } from "../../configs/environment";
+
 export default function HomePage() {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    axios.get(environment.baseURL + "/restaurants/").then((res) => res.data)
+  );
+
+  //const { Load, Err, Data } = RestaurantsService.GetARestaurant();
+
+  if (data) {
+    console.log(data);
+  }
+
   return <h1>It Worked..!</h1>;
 }
