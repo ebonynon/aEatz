a82ec3c834194f8d8b5fbd51dd46061774a2d267
ebonynon <sithumsandeepap@gmail.com>
2020-12-10 03:13:53 +0530

    restaurant add axios post

--- src/services/restaurants.service.js
+++ src/services/restaurants.service.js
@@ -1,11 +1,12 @@
-import Axios from "axios";
+import axios from "axios";
 import { environment } from "../configs/environment";
 
 // Get a Restaurant
 export function GetARestaurant() {
   var array = [];
 
-  Axios.get(environment.baseURL + "restaurants/")
+  axios
+    .get(environment.baseURL + "/restaurants/")
     .then((e) => {
       array = e.data;
       console.log(array);
@@ -17,4 +18,8 @@ export function GetARestaurant() {
 
 export function AddARestaurant(data) {
   console.log(data);
+  axios
+    .post(environment.baseURL + "/restaurants/", data)
+    .then((res) => console.log(res.status))
+    .catch((err) => console.log(err.message));
 }
