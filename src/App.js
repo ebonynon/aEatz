84f053c37b84aea04c86f6f54095c338b2254670
ebonynon <sithumsandeepap@gmail.com>
2020-12-09 06:39:47 +0530

    switch to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { createBrowserHistory } from "history";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // pages for this product
 import HomePage from "./pages/HomePage/HomePage";
@@ -12,23 +11,19 @@ import CustomersPage from "./pages/CustomersPage/CustomersPage";
 import NavBar from "./components/common/Navbar";
 import Parallax from "./components/common/Parallax";
 
-const hist = createBrowserHistory();
-
 function App() {
   return (
     <ChakraProvider>
-      <div>
+      <Parallax />
+      <BrowserRouter>
         <NavBar />
-        <Parallax />
-        <Router history={hist}>
-          <Switch>
-            <Route path="/" component={HomePage} />
-            <Route path="/orders" component={OrdersPage} />
-            <Route path="/customers" component={CustomersPage} />
-            <Route path="/food-items" component={FoodItemPage} />
-          </Switch>
-        </Router>
-      </div>
+        <Switch>
+          <Route path="/orders" component={OrdersPage} />
+          <Route path="/customers" component={CustomersPage} />
+          <Route path="/food-items" component={FoodItemPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
