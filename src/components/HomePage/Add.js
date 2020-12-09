7b896b133fae4fd859fbb0f675c037ebe4cafbb6
ebonynon <sithumsandeepap@gmail.com>
2020-12-09 19:01:24 +0530

    add rm color

--- src/components/HomePage/Add.js
+++ src/components/HomePage/Add.js
@@ -3,15 +3,11 @@ import {
   Grid,
   GridItem,
   Button,
-  colorMode,
   useDisclosure,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 export default function AddComponent() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const bgColor = { light: "gray.200", dark: "gray.500" };
-  const boxColor = { light: "teal.300", dark: "teal.600" };
-  const textColor = { light: "black", dark: "gray.100" };
   return (
     <Box
       borderWidth="1px"
@@ -19,8 +15,6 @@ export default function AddComponent() {
       margin={1}
       w="100%"
       p={1}
-      bg={bgColor[colorMode]}
-      color={textColor[colorMode]}
     >
       <Grid templateColumns="repeat(6, 1fr)" align="center" gap={6}>
         <GridItem colStart={6}>
