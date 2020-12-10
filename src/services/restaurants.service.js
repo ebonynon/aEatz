bd47e9d5d4fa2bf1fdd4bef4ec37d57ccb364a0e
ebonynon <sithumsandeepap@gmail.com>
2020-12-10 15:02:39 +0530

    rm getarestaurant

--- src/services/restaurants.service.js
+++ src/services/restaurants.service.js
@@ -1,21 +1,6 @@
 import axios from "axios";
 import { environment } from "../configs/environment";
 
-// Get a Restaurant
-export function GetARestaurant() {
-  var array = [];
-
-  axios
-    .get(environment.baseURL + "/restaurants/")
-    .then((e) => {
-      array = e.data;
-      console.log(array);
-      return array;
-    })
-    .catch((err) => console.error(err));
-  return array;
-}
-
 export function AddARestaurant(data) {
   console.log(data);
   axios
