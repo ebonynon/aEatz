5bf63810cae9db5f1fc604a563a262376b1774b3
ebonynon <sithumsandeepap@gmail.com>
2020-12-10 17:52:56 +0530

    clean parallax n change blur 1px

--- src/components/common/Parallax.js
+++ src/components/common/Parallax.js
@@ -1,11 +1,6 @@
-import { Flex, Box, Image, useColorMode } from "@chakra-ui/react";
+import { Image, useColorMode } from "@chakra-ui/react";
 
 export default function Parallax() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = { light: "gray.300", dark: "gray.600" };
-  const textColor = { light: "black", dark: "gray.100" };
-  const coverImg = { filter: "blur(8px)" };
-
   return (
     <Image
       h="20vh"
@@ -13,7 +8,7 @@ export default function Parallax() {
       src="https://cdn.asiatatler.com/asiatatler/i/hk/2020/02/12125015-louise-hong-kong-instagrammable-restaurants-2020-hong-kong-tatler_cover_1800x1200.jpg"
       alt="Cover Image"
       objectFit="cover"
-      filter="blur(2px)"
+      filter="blur(1px)"
     />
   );
 }
