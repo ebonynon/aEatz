0016df42f9b7101f4f60a3df83c558efc3b5c046
ebonynon <sithumsandeepap@gmail.com>
2020-12-11 09:53:47 +0530

    customers service ~0

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 import * as RestaurantsService from "./restaurants.service";
 import * as FoodItemsService from "./foodItems.service";
+import * as CustomersService from "./customers.service";
 
 export { RestaurantsService };
 export { FoodItemsService };
+export { CustomersService };
