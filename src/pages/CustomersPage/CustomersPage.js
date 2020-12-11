4d0a3e85c490c3dddc5e95aa58d10f3724a2e5ef
ebonynon <sithumsandeepap@gmail.com>
2020-12-11 10:20:20 +0530

    customer item add edit table ~0

--- src/pages/CustomersPage/CustomersPage.js
+++ src/pages/CustomersPage/CustomersPage.js
@@ -1,3 +1,35 @@
+import axios from "axios";
+import { Box, Flex } from "@chakra-ui/react";
+import { useQuery } from "react-query";
+import { environment } from "../../configs/environment";
+import TableComponent from "../../components/Customers/Table";
+import AddComponent from "../../components/Customers/Add";
+
 export default function CustomersPage() {
-  return <h1>It CustomersPage Worked..!</h1>;
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    axios.get(environment.baseURL + "/customers/").then((res) => res.data)
+  );
+
+  let table;
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  if (!data) {
+    if (isLoading) {
+      table = <h1>Nop</h1>;
+    }
+  } else {
+    table = Object.values(data).map((value) => <TableComponent vl={value} />);
+  }
+
+  return (
+    <Flex justify="center" align="center" p={4}>
+      <Box p={1} color="white">
+        <AddComponent />
+        {table}
+      </Box>
+    </Flex>
+  );
 }
