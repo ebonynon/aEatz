6b1261ee6f4397965104fc6e5cc02ace58291c08
ebonynon <sithumsandeepap@gmail.com>
2020-12-15 18:01:09 +0530

    fix env to take from file

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,8 +1,5 @@
-const process = require("process");
-
-const URL = process.env.BASEURL;
-var bURL = "https://stormy-depths-99062.herokuapp.com/v1";
-//bURL: 'http://localhost:3080/v1'
+const URL = process.env.REACT_APP_BASEURL;
+let bURL = "https://stormy-depths-99062.herokuapp.com/v1";
 
 if (URL) {
   bURL = URL;
